1a32211809cad7dc61eacf8c5e7fbe202248f3b9
karlafernandes <karla.fernandes@gmail.com>
2018-04-11 14:27:13 +0200

    Add: Google Tag Manager + Optimize solve issues #2 #3

--- startit/header.php
+++ startit/header.php
@@ -26,15 +26,24 @@
   <link href="//static2cdn.hubspot.com/hubspot.com/static-1.4334/_hsg-ss/build/icons/hsg-icons.css" rel="stylesheet">
   <link href="//static2cdn.hubspot.com/hubspot.com/static-1.4334/_hsg-ss/build/css/hsg.css" rel="stylesheet">
   <link href="/wp-content/themes/startit/assets/css/makersite.css" rel="stylesheet">
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments)};
-    gtag('js', new Date());
-  
-    gtag('config', 'UA-76984622-1');
-  </script>
+
+<!-- Google Analytics + Optimize -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-76984622-1', 'auto');
+  ga('require', 'GTM-K5Z2LS2');
+  ga('send', 'pageview');
+</script>
+
+<!-- Google Tag Manager -->
+<script>
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push( {'gtm.start': new Date().getTime(),event:'gtm.js'} );var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-5M2G9QN');
+</script>
 	
-	<!-- Hotjar Tracking Code for staging.makersite.net -->
+<!-- Hotjar Tracking Code for staging.makersite.net -->
 <script>
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
@@ -49,6 +58,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5M2G9QN" height="0" width="0" style="display:none;visibility:hidden"></iframe>
 <?php qode_startit_get_side_area(); ?>
 
 <div class="qodef-wrapper">
