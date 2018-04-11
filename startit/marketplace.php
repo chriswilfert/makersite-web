df011beb5a0aff55046309cff7c21ddeca52e0b2
karlafernandes <karla.fernandes@gmail.com>
2018-04-11 13:20:51 +0200

    Fix: MarketSite template

--- startit/marketplace.php
+++ startit/marketplace.php
@@ -5,7 +5,6 @@
 ?>
 <?php get_header(); ?>
 <?php if (have_posts()) : ?>
-<?php while (have_posts()) : the_post(); ?>
 <?php get_template_part( 'title' ); ?>
 <?php get_template_part('slider'); ?>
 	<div class="qodef-container">
@@ -15,6 +14,4 @@
 		</div>
 		<?php do_action('qode_startit_before_container_close'); ?>
 	</div>
-<?php endwhile; ?>
-<?php endif; ?>
 <?php get_footer(); ?>
