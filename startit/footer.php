6619c54f31fc8dfac5d103477b5b2d2e10e3e2c2
karlafernandes <karla.fernandes@gmail.com>
2018-05-10 16:04:06 +0100

    Adding Featured Webinars + Posts Issues #15 #5

--- startit/footer.php
+++ startit/footer.php
@@ -2,6 +2,30 @@
 
 qode_startit_get_footer();
 
+?>
+
+<div class="web_blog">
+<div class="column_1">
+    <h2><span>UPCOMING<span> WEBINAR</h2>
+    <a href="#">
+        &quote;Circular Economy: New Developments And Success Factors For Businesses With Industrial, Technical And Durable Products&quote;
+        <span class="date">Wednesday, May 9, 2018 03:00 PM Europe/London</span>
+    </a>
+    <a href="" class="bt_cta">Learn more</a>
+</div>
+<div class="column_2">
+<h2><span>LATEST</span> POSTS</h2>
+    <div class="latest_blog_posts">
+        <a href="#">Your new toolkit for sustainable products</a>
+        <a href="#">New supply chain tool helps businesses unearth the climate impact of their operations</a>
+        <a href="#">Makersite Offers Faster Analysis of Product Sustainability, Costs, Supply Chain Stewardship</a>
+        <a href="#">Digital tool maps the environmental impact of individual supply chains</a>
+        <a href="#">Makersite Introduces Apps for Faster Ecodesign, Stewardship and Cost Analysis </a>
+     </div>
+</div>
+
+<?php
+
 global $qode_startit_toolbar;
 if(isset($qode_startit_toolbar)) include("toolbar.php");
 
